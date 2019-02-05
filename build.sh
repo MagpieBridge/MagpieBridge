456015fa70cfdd83c93b48886a32a585e0bde70a
Julian Dolby <juliandolby@gmail.com>
2019-02-05 06:34:18 -0500

    repository

---
+++ build.sh
@@ -0,0 +1,21 @@
+
+function install {
+ jarFile=$1
+ pomFile=`dirname $jarFile`/`basename $jarFile .jar`.pom
+ cmd="mvn install:install-file -DpomFile=$pomFile -Dfile=$jarFile -Dpackaging=jar -DlocalRepositoryPath=repository"
+
+ sourcesFile=`dirname $jarFile`/`basename $jarFile .jar`-sources.jar
+ if [ -e $sourcesFile ]; then
+   cmd="$cmd -Dsources=$sourcesFile"
+ fi
+
+ $cmd
+}
+
+function install_tree {
+    for f in `find $1 -name '*-SNAPSHOT.jar'`; do
+	install $f
+    done
+}
+
+install_tree $1
