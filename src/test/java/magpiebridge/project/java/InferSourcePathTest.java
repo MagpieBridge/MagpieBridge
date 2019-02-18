6210260dba76c6b78c501ca0942f9fc80a07ed56
Linghui Luo <linghui@mail.upb.de>
2019-02-18 16:06:12 +0100

    update InferSourcePathTest

--- src/test/java/magpiebridge/project/java/InferSourcePathTest.java
+++ src/test/java/magpiebridge/project/java/InferSourcePathTest.java
@@ -1,31 +1,35 @@
 package magpiebridge.project.java;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class InferSourcePathTest {
 	@Test
 	public void test1() {
-		Path root = Paths.get("./src/test/resources/cryptoDemoTest/").toAbsolutePath();
+		Path root = Paths.get("src/test/resources/cryptoDemoTest/").toAbsolutePath();
 		Set<Path> sourcePath = InferSourcePath.sourcePath(root);
-		Path expected = Paths.get("./src/test/resources/cryptoDemoTest/src").toAbsolutePath();
+		Path expected = Paths.get("src/test/resources/cryptoDemoTest/src").toAbsolutePath();
 		assertEquals(expected.toString(), sourcePath.iterator().next().toString());
 	}
-	
-	@Ignore
+
+	@Test
 	public void test2() {
-		Path root = Paths.get("").toAbsolutePath();
-		System.out.println(root);
+		Path root = Paths.get("src").toAbsolutePath();
 		Set<Path> sourcePath = InferSourcePath.sourcePath(root);
+		Path expected = Paths.get("src/main/java").toAbsolutePath();
+		boolean found = false;
 		for (Path p : sourcePath) {
-			System.out.println(p.toString());
+			if (expected.equals(p)) {
+				found = true;
+			}
 		}
+		assertTrue(found);
 	}
-	
+
 }
