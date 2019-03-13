f705c0c516d26c237e71428ca9117c3369f87db6
Linghui Luo <linghui@mail.upb.de>
2019-03-13 13:10:55 +0100

    format code

--- src/test/java/magpiebridge/project/java/JavaProjectServiceTest.java
+++ src/test/java/magpiebridge/project/java/JavaProjectServiceTest.java
@@ -6,12 +6,11 @@ import static org.junit.Assert.assertNotEquals;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import magpiebridge.core.JavaProjectService;
-
-import org.junit.Ignore;
+import org.junit.Test;
 
 public class JavaProjectServiceTest {
 
-  @Ignore
+  @Test
   public void testMavenProject() {
     JavaProjectService ps = new JavaProjectService();
     Path root = Paths.get("src/test/resources/DemoProject/").toAbsolutePath();
