e7b02e191c4f6605d6489ab454a5a076970a087c
Christian Brüggemann <mail@cbruegg.com>
2019-03-15 19:21:35 +0100

    Add MainActivity.java

--- src/test/resources/MyApplication/app/src/main/java/de/upb/demoapplication/MainActivity.java
+++ src/test/resources/MyApplication/app/src/main/java/de/upb/demoapplication/MainActivity.java
@@ -1,8 +1,7 @@
 package de.upb.demoapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
