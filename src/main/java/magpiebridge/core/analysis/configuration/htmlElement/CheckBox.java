9dfe88ecf7e6ed8b54ac4cbd49de5258ffe1fc00
Ranjith K <ranjith.masthikatte@gmail.com>
2020-07-30 13:44:41 +0200

    By default, isChildSelecatable feature is enabled and value to the checkbox can be set in constructor now.
    Updated the format
    
    Update CheckBox.java

--- src/main/java/magpiebridge/core/analysis/configuration/htmlElement/CheckBox.java
+++ src/main/java/magpiebridge/core/analysis/configuration/htmlElement/CheckBox.java
@@ -5,49 +5,52 @@ import magpiebridge.core.analysis.configuration.OptionType;
 
 /**
  * This class represents the Checkbox with the below additional features.
- * 1. isChildSelectable - if true, then by selecting the parent checkbox, all its children also will be selected.
+ *
+ * <p>1. isChildSelectable - if true, then by selecting the parent checkbox, all its children also
+ * will be selected.
  *
  * @author Ranjith Krishnamurthy
  */
 public class CheckBox extends ConfigurationOption {
-    private boolean isChildSelectable;
+  private boolean isChildSelectable;
 
-    /**
-     * This is the getter for isChildSelectable.
-     *
-     * @return Boolean - isChildSelectable
-     */
-    public boolean isChildSelectable() {
-        return isChildSelectable;
-    }
+  /**
+   * This is the getter for isChildSelectable.
+   *
+   * @return Boolean - isChildSelectable
+   */
+  public boolean isChildSelectable() {
+    return isChildSelectable;
+  }
 
-    /**
-     * This is the setter for isChildSelectable
-     *
-     * @param childSelectable boolean - isChildSelectable
-     */
-    public void setChildSelectable(boolean childSelectable) {
-        isChildSelectable = childSelectable;
-    }
+  /**
+   * This is the setter for isChildSelectable
+   *
+   * @param childSelectable boolean - isChildSelectable
+   */
+  public void setChildSelectable(boolean childSelectable) {
+    isChildSelectable = childSelectable;
+  }
 
-    /**
-     * This constructs the Checkbox with default isChildSelectable to true.
-     *
-     * @param name Name of the Checkbox
-     */
-    public CheckBox(String name) {
-        super(name, OptionType.checkbox);
-        this.isChildSelectable = true;
-    }
+  /**
+   * This constructs the Checkbox with default isChildSelectable to true.
+   *
+   * @param name Name of the Checkbox
+   */
+  public CheckBox(String name) {
+    super(name, OptionType.checkbox);
+    this.isChildSelectable = true;
+  }
 
-    /**
-     * This contructs the checkbox with the isChildSelectable provided.
-     *
-     * @param name              Name of the Checkbox
-     * @param isChildSelectable boolean - isChildSelectable
-     */
-    public CheckBox(String name, boolean isChildSelectable) {
-        super(name, OptionType.checkbox);
-        this.isChildSelectable = isChildSelectable;
-    }
+  /**
+   * This contructs the checkbox with the isChildSelectable provided.
+   *
+   * @param name Name of the Checkbox
+   * @param value Value to set the checkbox on or off. To set the checkbox on value should be "true"
+   *     or "on".
+   */
+  public CheckBox(String name, String value) {
+    super(name, OptionType.checkbox, value);
+    this.isChildSelectable = true;
+  }
 }
