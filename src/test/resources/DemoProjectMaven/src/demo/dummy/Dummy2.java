d8ca4898de06027a10fc0121fbd54d6897623f9a
Ranjith K <ranjith.masthikatte@gmail.com>
2020-08-13 13:10:26 +0200

    Updated test case to check that Magpie gives all the classes (sourceClassFullQualifiedNames) in the Project.

---
+++ src/test/resources/DemoProjectMaven/src/demo/dummy/Dummy2.java
@@ -0,0 +1,7 @@
+package demo.dummy;
+
+public class Dummy2 {
+    public static void main(String...args) {
+        System.out.println("Its a dummy class to test the sourceClassFullQualifiedNames counts");
+    }
+}
