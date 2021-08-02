8e6b10ac01824fa9d9ca0f2739a41f7f59293b2b
Sami <sami.cseseu@gmail.com>
2021-08-02 14:18:19 +0200

    Fixed bug and added checks for null element.

--- src/main/java/magpiebridge/core/analysis/configuration/SARIFElement.java
+++ src/main/java/magpiebridge/core/analysis/configuration/SARIFElement.java
@@ -12,25 +12,26 @@ import magpiebridge.core.AnalysisResult;
 import magpiebridge.core.Kind;
 import org.eclipse.lsp4j.DiagnosticSeverity;
 
-/**
- * Converts SARIF JSON to analysis results
- *
- */
+/** Converts SARIF JSON to analysis results */
 public class SARIFElement {
   private List<AnalysisResult> analysisResults;
 
   public SARIFElement(JsonObject sarif) throws MalformedURLException {
     this.analysisResults = new ArrayList<AnalysisResult>();
-    JsonArray runs = sarif.get("runs").getAsJsonArray();
-    JsonArray results = new JsonArray();
-    JsonObject run = new JsonObject();
-    JsonObject result = new JsonObject();
-    for (int i = 0; i < runs.size(); i++) {
-      run = runs.get(i).getAsJsonObject();
-      results = run.get("results").getAsJsonArray();
-      for (int j = 0; j < results.size(); j++) {
-        result = results.get(j).getAsJsonObject();
-        insertAnalysisResult(result);
+    if (notNullAndHas(sarif, "runs")) {
+      JsonArray runs = sarif.get("runs").getAsJsonArray();
+      JsonArray results = new JsonArray();
+      JsonObject run = new JsonObject();
+      JsonObject result = new JsonObject();
+      for (int i = 0; i < runs.size(); i++) {
+        run = runs.get(i).getAsJsonObject();
+        if (notNullAndHas(run, "results")) {
+          results = run.get("results").getAsJsonArray();
+          for (int j = 0; j < results.size(); j++) {
+            result = results.get(j).getAsJsonObject();
+            insertAnalysisResult(result);
+          }
+        }
       }
     }
   }
@@ -40,49 +41,70 @@ public class SARIFElement {
   }
 
   private void insertAnalysisResult(JsonObject result) throws MalformedURLException {
-    // TODO: Ensuring check for not present parameter according to the required list
     JsonObject message = result.get("message").getAsJsonObject();
     JsonArray locations = result.get("locations").getAsJsonArray();
-    String messageText = message.get("text").getAsString();
+    String messageText = notNullAndHas(message, "text") ? message.get("text").getAsString() : null;
     Position errorPosition = locationToPosition(locations.get(0).getAsJsonObject());
+
+    if (errorPosition == null || messageText == null) {
+      return;
+    }
+
     List<Pair<Position, String>> relatedInfo = new ArrayList<>();
     String code = locationToCode(locations.get(0).getAsJsonObject());
-    JsonArray codeFlows = result.get("codeFlows").getAsJsonArray();
-    JsonObject codeFlow = new JsonObject();
-    JsonArray threadFlows = new JsonArray();
-    JsonObject threadFlow = new JsonObject();
-    JsonArray threadFlowLocations = new JsonArray();
 
-    for (int i = 0; i < codeFlows.size(); i++) {
-      codeFlow = codeFlows.get(i).getAsJsonObject();
-      threadFlows =
-          notNullAndHas(codeFlow, "threadFlows")
-              ? codeFlow.get("threadFlows").getAsJsonArray()
-              : null;
-      for (int j = 0; j < threadFlows.size(); j++) {
-        threadFlow = threadFlows.get(j).getAsJsonObject();
-        threadFlowLocations =
-            notNullAndHas(threadFlow, "locations")
-                ? threadFlow.get("locations").getAsJsonArray()
+    if (notNullAndHas(result, "codeFlows")) {
+      JsonArray codeFlows = result.get("codeFlows").getAsJsonArray();
+      JsonObject codeFlow = new JsonObject();
+      JsonArray threadFlows = new JsonArray();
+      JsonObject threadFlow = new JsonObject();
+      JsonArray threadFlowLocations = new JsonArray();
+      for (int i = 0; i < codeFlows.size(); i++) {
+        codeFlow = codeFlows.get(i).getAsJsonObject();
+        threadFlows =
+            notNullAndHas(codeFlow, "threadFlows")
+                ? codeFlow.get("threadFlows").getAsJsonArray()
                 : null;
-        if (threadFlowLocations != null) {
-          relatedInfo = locationsToFlow(threadFlowLocations);
-          if (code == null && relatedInfo != null && relatedInfo.size() != 0) {
-            int lastLocation = relatedInfo.size() - 1;
-            code = relatedInfo.get(lastLocation).snd;
-          }
+        if (threadFlows != null) {
+          for (int j = 0; j < threadFlows.size(); j++) {
+            threadFlow = threadFlows.get(j).getAsJsonObject();
+            threadFlowLocations =
+                notNullAndHas(threadFlow, "locations")
+                    ? threadFlow.get("locations").getAsJsonArray()
+                    : null;
+            if (threadFlowLocations != null) {
+              relatedInfo = locationsToFlow(threadFlowLocations);
+              if (code == null && relatedInfo != null && relatedInfo.size() != 0) {
+                int lastLocation = relatedInfo.size() - 1;
+                code = relatedInfo.get(lastLocation).snd;
+              }
 
-          analysisResults.add(
-              new SARIFResult(
-                  Kind.Diagnostic,
-                  errorPosition,
-                  messageText,
-                  relatedInfo,
-                  DiagnosticSeverity.Error,
-                  null,
-                  code));
+              analysisResults.add(
+                  new SARIFResult(
+                      Kind.Diagnostic,
+                      errorPosition,
+                      messageText,
+                      relatedInfo,
+                      DiagnosticSeverity.Error,
+                      null,
+                      code));
+            }
+          }
         }
       }
+    }
+
+    // if the analysis result is empty add error with error position and message.
+    if (analysisResults.isEmpty()) {
+      analysisResults.add(
+          new SARIFResult(
+              Kind.Diagnostic,
+              errorPosition,
+              messageText,
+              relatedInfo,
+              DiagnosticSeverity.Error,
+              null,
+              code));
     }
   }
 
@@ -106,10 +128,11 @@ public class SARIFElement {
   }
 
   private Position locationToPosition(JsonObject location) throws MalformedURLException {
-    JsonObject phsicalLocation =
-        notNullAndHas(location, "physicalLocation")
-            ? location.getAsJsonObject("physicalLocation")
-            : null;
+    if (!notNullAndHas(location, "physicalLocation")) {
+      return null;
+    }
+
+    JsonObject phsicalLocation = location.getAsJsonObject("physicalLocation");
     JsonObject artifactLocation =
         notNullAndHas(phsicalLocation, "artifactLocation")
             ? phsicalLocation.getAsJsonObject("artifactLocation")
@@ -130,7 +153,8 @@ public class SARIFElement {
   }
 
   private String locationToCode(JsonObject location) {
-    JsonObject message = location.has("message") ? location.getAsJsonObject("message") : null;
+    JsonObject message =
+        notNullAndHas(location, "message") ? location.getAsJsonObject("message") : null;
     String text = notNullAndHas(message, "text") ? message.get("text").toString() : null;
     return text;
   }
